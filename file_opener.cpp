d0d9851adf83f25f997c6c379efe1489b87a5993
Krishna Saglani <b23cs1027@iitj.ac.in>
2025-03-19 15:36:36 +0530

    Made basic file opener..Now ready for scanner code

---
+++ file_opener.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <stdio.h>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    char * source_file = argv[1];
+
+    FILE * fp;
+    fp = fopen(source_file, "r");
+
+    if(!fp)
+    {
+        cout<<"Failed to open file";
+        exit(1);
+    }
+    else
+    {
+        fseek(fp, 0, SEEK_END);
+        int file_size = ftell(fp);
+        // In c++, ftell is used to calculate size of file, in this way
+        fseek(fp,0,SEEK_SET);
+        // bringing fp back to start of file
+
+
+        char * buffer = NULL; 
+        buffer = new (nothrow) char[file_size+1];
+
+        if(!buffer) 
+        {
+            cout<<"Allocation of buffer memory failed\n";
+        }
+        else
+        {
+            fread(buffer, sizeof(char), file_size, fp);
+            // contents of test.cpp copied into buffer
+            // scanner(buffer); 
+            // to be defined soon
+
+            cout<<"Compiled Successfully!";
+        }
+        delete buffer;
+        fclose(fp);
+
+    }
+
+    return 0;
+
+}
+
---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Syntaxia";
+    return 0;
+}
+
