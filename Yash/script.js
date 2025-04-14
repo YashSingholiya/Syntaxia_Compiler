02f544fb3c3267867e7d3d19203defcc766346f9
YashSingholiya <b23cs1081@iitj.ac.in>
2025-04-15 00:50:43 +0530

    Create script.js

---
+++ Yash/script.js
@@ -0,0 +1,31 @@
+let editor;
+
+// Load Monaco Editor
+require.config({ paths: { vs: 'https://unpkg.com/monaco-editor@latest/min/vs' } });
+require(['vs/editor/editor.main'], function () {
+  editor = monaco.editor.create(document.getElementById('editor'), {
+    value: '// Write your C++ code here\n',
+    language: 'cpp',
+    theme: 'vs-dark'
+  });
+});
+
+// Function to compile the code
+function compileCode() {
+    const code = editor.getValue();
+  
+    fetch('http://127.0.0.1:5000/compile', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ code })
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log("Backend Response:", data); // Add this line
+  
+      document.getElementById('lexical-output').innerText = data.lexical || 'No output';
+      document.getElementById('semantic-output').innerText = data.semantic || 'No output';
+      document.getElementById('assembly-output').innerText = data.assembly || 'No output';
+    })
+    .catch(err => alert('Compilation error: ' + err));
+  }
